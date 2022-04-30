ce89913429bfc27bb8e0656b354843d865cd3ccd
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-30 05:33:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+var protoUrl = "tg:resolve?domain=KoshikKumar17";
+if (false) {
+  var iframeContEl = document.getElementById('tgme_frame_cont') || document.body;
+  var iframeEl = document.createElement('iframe');
+  iframeContEl.appendChild(iframeEl);
+  var pageHidden = false;
+  window.addEventListener('pagehide', function () {
+    pageHidden = true;
+  }, false);
+  window.addEventListener('blur', function () {
+    pageHidden = true;
+  }, false);
+  if (iframeEl !== null) {
+    iframeEl.src = protoUrl;
+  }
+var tme_bg = document.getElementById('tgme_background');
+if (tme_bg) {
+  TWallpaper.init(tme_bg);
+  TWallpaper.animate(true);
+  window.onfocus = function(){ TWallpaper.update(); };
+}
+document.body.classList.remove('no_transition');
+
+function toggleTheme(dark) {
+  document.documentElement.classList.toggle('theme_dark', dark);
+  window.Telegram && Telegram.setWidgetOptions({dark: dark});
+}
+if (window.matchMedia) {
+  var darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+  toggleTheme(darkMedia.matches);
+  darkMedia.addListener(function(e) {
+    toggleTheme(e.matches);
+  });
+}
